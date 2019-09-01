0d74c43a6b5a4e7028eaf83c155e77453b6a952d
Sam Stephens <sam.stephens@10xbanking.com>
2019-09-01 17:47:12 +0100

    Added correct update and create functionality

--- kafkaconnector-operator.py
+++ kafkaconnector-operator.py
@@ -4,11 +4,20 @@ import requests
 
 @kopf.on.create("sdjs.dev", "v1", "kafkaconnectors")
 def create_fn(body, spec, **kwargs):
-    print(f"A handler is called with body: {body}")
+    print(f"Create handler called with body: {body}")
+    create_or_update(body, spec)
+
+
+@kopf.on.update("sdjs.dev", "v1", "kafkaconnectors")
+def update_fn(body, spec, **kwargs):
+    print(f"Update handler called with body: {body}")
+    create_or_update(body, spec)
+
+
+def create_or_update(body, spec):
     connector_name = body['metadata']['name']
     resp = requests.put(f"http://localhost:8083/connectors/{connector_name}/config",
-                        data=convert_config_params(spec['config']),
-                        headers={"Content-Type": "application/json"})
+                        json=convert_config_params(spec['config']))
     print(f"{resp.status_code}: {resp.content}")
 
 
@@ -18,6 +27,7 @@ def convert_config_params(config):
 
 @kopf.on.delete("sdjs.dev", "v1", "kafkaconnectors")
 def delete_fn(body, **kwargs):
+    print(f"Delete handler called with body: {body}")
     connector_name = body['metadata']['name']
     resp = requests.delete(f"http://localhost:8083/connectors/{connector_name}")
     print(f"{resp.status_code}: {resp.content}")
